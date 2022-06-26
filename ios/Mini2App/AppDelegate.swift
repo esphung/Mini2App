cb54d49505366d8c8b56b845395310f0aadc5868
Eric Phung <esphung@gmail.com>
2022-06-25 19:41:20 -0500

    DJISDK App registering

--- ios/Mini2App/AppDelegate.swift
+++ ios/Mini2App/AppDelegate.swift
@@ -6,20 +6,60 @@
 //
 
 import Foundation
+import DJISDK
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, DJISDKManagerDelegate {
+  
+  //  TODO: Move to VC
+  func appRegisteredWithError(_ error: Error?) {
+      var message = "Register App Succeeded!"
+      if error != nil {
+          message = "DJISDK Register App Failed! Please enter your App Key in the plist file and check the network."
+          print(message)
+      } else {
+          print("DJISDK registerAppSuccess: \(message)")
+      }
+  }
+
+  //  TODO: Move to VC
+  func didUpdateDatabaseDownloadProgress(_ progress: Progress) {
+    print(progress)
+  }
+  //  TODO: Move to VC
+  func registerApp() {
+      DJISDKManager.registerApp(with: self)
+  }
+
   var window: UIWindow?
-  var bridge: RCTBridge!;
+
+  var bridge: RCTBridge!
+
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    let jsCodeLocation: URL;
-    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
-    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "Mini2App", initialProperties: nil, launchOptions: launchOptions)
+  
+    let jsCodeLocation: URL
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(
+      forBundleRoot: "index",
+      fallbackExtension: "jsbundle"
+    )
+
+    let rootView = RCTRootView(
+      bundleURL: jsCodeLocation,
+      moduleName: "Mini2App",
+      initialProperties: nil,
+      launchOptions: launchOptions
+    )
+
     let rootViewController = UIViewController()
     rootViewController.view = rootView;
+
     self.window = UIWindow(frame: UIScreen.main.bounds)
     self.window?.rootViewController = rootViewController
     self.window?.makeKeyAndVisible();
+    
+    //  TODO: Move to VC
+    registerApp()
+
     return true
   }
 }
